b930210cb229d9e4140e552be258f2c9ffe8c33b
santanubasu <santanu.basu@gmail.com>
2015-11-26 07:13:49 -0700

    Process arrays of operations into arrays of arrays of result operations, and single operations into arrays of result operations.

--- server.js
+++ server.js
@@ -10,19 +10,25 @@ var Dispatcher = Object.extend({
         return this;
     },
     route:function(operations, options) {
-        return q
-            .all(operations.map(function(operation) {
-                var registeredResource = shared.Resource.lookup(operation[config.syntax.metaKey]._r);
-                var operationKey = operation[config.syntax.metaKey]._op;
-                operationKey = operationKey||shared.Resource.patch;
-                return registeredResource[operationKey](operation)
-                    .fail(function(e) {
-                        // TODO
-                        // need to determine where to handle errors such as version mismatches, and how to return
-                        // meaningful errors
-                        console.log(e);
-                    })
-            }.bind(this)))
+        var process = function(operation) {
+            var registeredResource = shared.Resource.lookup(operation[config.syntax.metaKey]._r);
+            var operationKey = operation[config.syntax.metaKey]._op;
+            operationKey = operationKey||shared.Resource.patch;
+            return registeredResource[operationKey](operation)
+                .fail(function(e) {
+                    // TODO
+                    // need to determine where to handle errors such as version mismatches, and how to return
+                    // meaningful errors
+                    console.log(e);
+                })
+        }.bind(this);
+
+        if (_.isArray(operations)) {
+            return q.all(operations.map(process))
+        }
+        else {
+            return process(operations);
+        }
     }
 })
 
