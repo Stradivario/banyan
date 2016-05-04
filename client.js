538e279974ad00c9b9ba4f59adc70b0c1d43e631
santanubasu <santanu.basu@gmail.com>
2016-05-04 08:11:57 -0700

    Another attempt at traversal truncation, this time using block()

--- client.js
+++ client.js
@@ -96,14 +96,14 @@ var Store = Object.extend({
             if (this.key==="_observer") {
                 try {
                     value.discardChanges();
-                    this.stop();
+                    this.block();
                 }
                 catch (e) {
                     log.error(e);
                 }
             }
             else if (this.notRoot&&shared.Entity.isEntity(value)) {
-                this.stop();
+                this.block();
             }
         })
     },
